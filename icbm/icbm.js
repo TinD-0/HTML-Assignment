c40e084f1db84a8890b5076cce0b214a8096f731
TinD <tinduyphuoc.dao@student.tdsb.on.ca>
2025-01-07 09:26:20 -0500

    ICBM Page

---
+++ icbm/icbm.js
@@ -0,0 +1,8 @@
+function OnNukeButton()
+{
+    var body = document.getElementById("body");
+    body.style.background = "maroon";
+
+    var missile = document.getElementById("missile");
+    missile.style.display = "block";
+}
