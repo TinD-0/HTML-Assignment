cd3b46b5b0826f8e3fa368812c8a01a26b6c2db6
TinD <tinduyphuoc.dao@student.tdsb.on.ca>
2025-01-15 12:01:14 -0500

    Add another section of form

--- form/form.js
+++ form/form.js
@@ -6,3 +6,19 @@ function OnSubmit()
     p.innerText = "Successfully sent all information to the Chinese Communist Party";
     form.replaceWith(p);
 }
+
+function OnYes()
+{
+    var div = document.getElementById("yesno");
+    var p = document.createElement("p");
+    p.innerText = "Thank you for your good response. You will be spared 4 years of life.";
+    div.replaceWith(p);
+}
+
+function OnNo()
+{
+    var div = document.getElementById("yesno");
+    var p = document.createElement("p");
+    p.innerText = ":O\nHOW COULD YOU?!?\nThe Soviet Union will be notified of this.";
+    div.replaceWith(p);
+}
